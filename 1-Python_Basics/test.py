4965bb1d9293b2cb97ad5c8f2c107c640c2a8170
princerxj <pr273582@gmail.com>
2025-12-13 13:53:39 +0530

    Basic Syntaxes , semenatics and Operators

---
+++ 1-Python_Basics/test.py
@@ -0,0 +1,10 @@
+## This is a single line comment in Python . 
+'''
+This is a multi-line comment in Python , 
+Here we can write more than one line of comments in python.
+'''
+
+'''
+Single line comments work same in py and jupyter notebook , 
+but multiline comments in jupyter notebook are created using markdown cells.
+'''
